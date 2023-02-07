811bb99b3d6cf91774982ca408f96fa088c13b28
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2023-02-08 01:04:19 +0700

    Configuring cli

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,4 +1,52 @@
 import { build } from "../esbuild.config";
+import yargs from "yargs";
+
+const scriptname = "build-esbuild";
+
+const usage = `${scriptname} --source=<src> --output=<output> [options]`;
+
+const options = yargs
+  .scriptName(scriptname)
+  .usage(usage)
+  .options("s", {
+    alias: ["src", "source", "srcDir", "sourceDir"],
+    demandOption: true,
+    default: "src",
+    describe: "The source folder",
+    type: "string",
+  })
+  .options("o", {
+    alias: ["out", "output", "outDir", "outputDir"],
+    demandOption: true,
+    default: "build",
+    describe: "The output folder",
+    type: "string",
+  })
+  .options("f", {
+    alias: ["fmt", "format"],
+    default: "cjs",
+    describe: "Options of the format of the transpiled files",
+    type: "string",
+  })
+  .options("m", {
+    alias: ["mn", "minify"],
+    default: false,
+    describe: "Option to minify the output files",
+    type: "boolean",
+  })
+  .options("d", {
+    alias: ["del", "clear", "clearPreviousBuild"],
+    default: true,
+    describe: "Options to delete the old build folder",
+    type: "boolean",
+  })
+  .options("v", {
+    alias: ["verbose"],
+    default: false,
+    describe: "Options to print steps verbosely",
+    type: "boolean",
+  })
+  .help(true).argv;
 
 async function run() {
   await new Promise<boolean>((resolve, reject) => {
